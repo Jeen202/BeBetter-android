8009dbe9243544452c8aaafd009737ecd06b1d27
LXXueshe <1501150463@qq.com>
2020-06-04 11:53:13 +0800

    个人资料界面 修改头像界面

--- app/src/main/java/cn/edu/scujcc/bebetter_android/MainActivity.java
+++ app/src/main/java/cn/edu/scujcc/bebetter_android/MainActivity.java
@@ -10,5 +10,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        <Object permission;
+        uses-permission Object name;
+        android:name="android.permission.CAMERA" />
+        <uses-permission android:name="android.permission.WRITE_EXTERNAL_STORAGE" />
+        <uses-permission android:name="android.permission.MOUNT_UNMOUNT_FILESYSTEMS" />
+        <uses-permission android:name="android.permission.INTERNET" />
     }
 }
