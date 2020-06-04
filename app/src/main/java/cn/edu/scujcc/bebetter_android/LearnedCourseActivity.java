758c40464fbd975687566dae46d04ad3ffe0c13f
Macy <64026499+Macy-Yang@users.noreply.github.com>
2020-06-04 09:47:48 +0800

    添加了学过的activity和学过的课程xml文件，并且在manifest中添加了activity intent

---
+++ app/src/main/java/cn/edu/scujcc/bebetter_android/LearnedCourseActivity.java
@@ -0,0 +1,14 @@
+package cn.edu.scujcc.bebetter_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LearnedCourseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_learned_course);
+    }
+}
